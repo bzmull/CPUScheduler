46772e0e4b01c54708c7078cbfb16a6f2bc9c3cf
bzmull <bmuller94@gmail.com>
2019-06-25 12:10:43 -0400

    test commit

---
+++ cmake-build-debug/CMakeFiles/CPUScheduler.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CPUScheduler.dir/main.cpp.o"
+  "CPUScheduler.pdb"
+  "CPUScheduler"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CPUScheduler.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
